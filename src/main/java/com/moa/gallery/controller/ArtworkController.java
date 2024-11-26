6b3a4f81d2be32236dd0b15c4d88c07c2d752849
KIJ9801 <154866994+KIJ9801@users.noreply.github.com>
2024-11-26 09:27:01 +0900

    갤러리

--- src/main/java/com/moa/gallery/controller/ArtworkController.java
+++ src/main/java/com/moa/gallery/controller/ArtworkController.java
@@ -3,6 +3,7 @@ package com.moa.gallery.controller;
 import com.moa.entity.Artwork;
 import com.moa.gallery.service.ArtworkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +21,17 @@ public class ArtworkController {
         @RequestParam(required = false) String subject,
         @RequestParam(required = false) String type,
         @RequestParam(required = false) String category,
+        @RequestParam(required = false) String search,
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "8") int size) {
-        return artworkService.getArtworks(subject, type, category, page, size);
+        return artworkService.getArtworks(subject, type, category, search ,page, size);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artwork> getArtworkById(@PathVariable Long id) {
+        return artworkService.getArtworkById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
 }
